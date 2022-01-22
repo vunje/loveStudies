900875b095ed30aedae44980b7470baaf76bb9ea
* <vunjetech@gmail.com>
2022-01-22 20:56:58 -0300

    makefile funcionando

--- 00_helloWorld/main.lua
+++ 00_helloWorld/main.lua
@@ -1,3 +1,9 @@
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Hello World!!!", 400, 300)
+end
+
+function love.keyreleased(key)
+    if key=="escape" then
+        love.event.quit()
+    end
 end
