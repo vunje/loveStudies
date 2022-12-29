0df37575b20fdcd8682d04a8e196222004d39d90
vunje <vunjetech@gmail.com>
2022-12-29 14:34:34 -0300

    teste e mais testes

--- 00_helloWorld/main.lua
+++ 00_helloWorld/main.lua
@@ -1,22 +1,22 @@
--- local Middleclass = require('plugin/HooECS/lib/middleclass')
--- local hooecs = require('plugin/HooECS')
--- hooecs.initialize({debug=true})
+local Middleclass = require('plugin/HooECS/lib/middleclass')
+local hooecs = require('plugin/HooECS')
+hooecs.initialize({debug=true})
 
--- local entity=hooecs.Entity()
--- local system=hooecs.System()
--- local engine=hooecs.Engine()
--- local component = hooecs.Component
--- local eventManager=hooecs.EventManager()
+local entity=hooecs.Entity()
+local system=hooecs.System()
+local engine=hooecs.Engine()
+local component = hooecs.Component
+local eventManager=hooecs.EventManager()
 
--- local class=hooecs.class
+local class=hooecs.class
 
 function love.load()
-    -- local Position=component.create("position",{"x","y"},{x=0,y=0})
+    local Position=component.create("position",{"x","y"},{x=0,y=0})
 end
 
 function love.draw()
-    a=7
-    b=5
+    a=8
+    b=8
     love.graphics.print(a+b, 400, 300)
     love.graphics.circle("fill", 230, 210, 100, 25)
     -- love.graphics.print("Hello World!!", 400, 300)
